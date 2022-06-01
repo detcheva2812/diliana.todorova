aa5ca259914b059e96ead7ad4062d39458b15b01
detcheva2812 <detcheva2812@gmail.com>
2022-06-01 14:00:44 +0300

    Homework4Methods

---
+++ src/Homework4/IsEvenMethod.java
@@ -0,0 +1,17 @@
+package Homework4;
+
+import java.util.Scanner;
+
+public class IsEvenMethod {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(isEven(a));
+
+    }
+
+    public static boolean isEven (int a){
+        return a % 2 == 0;
+    }
+}
